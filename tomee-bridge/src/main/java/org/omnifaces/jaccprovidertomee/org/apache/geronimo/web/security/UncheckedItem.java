c01b57712119e7ed73aa1d26e6ca60e843490195
Guillermo González de Agüero <z06.guillermo@gmail.com>
2018-07-09 16:58:02 +0200

    Added Geronimo atributions

--- tomee-bridge/src/main/java/org/omnifaces/jaccprovidertomee/org/apache/geronimo/web/security/UncheckedItem.java
+++ tomee-bridge/src/main/java/org/omnifaces/jaccprovidertomee/org/apache/geronimo/web/security/UncheckedItem.java
@@ -1,5 +1,6 @@
 /*
  * Copyright 2018 OmniFaces.
+ * Copyright 2003-2011 The Apache Software Foundation.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
